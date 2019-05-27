9c1b80657b8f5c4b4bdd124571906a0203a41c68
lsjsawyer <1159200202@qq.ccom>
2019-05-27 10:24:56 +0800

    new scripts

---
+++ test_01.py
@@ -0,0 +1,15 @@
+import time
+
+import allure
+import pytest
+
+
+class Test_002:
+
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
+    @allure.step(title="这是放入截图的测试步骤")
+    def test_add_png(self):
+        print("我是截图")
+        with open("C:\\Users\\86131\\Desktop\\untitled1\\scripts\\rooo.png", "rb") as f:
+            allure.attach("我是超人{}".format(time.strftime('%Y%m%r_%H%M%S')), f.read(), allure.attach_type.PNG)
+        assert True
